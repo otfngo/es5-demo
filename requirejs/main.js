004be604d614449d4b8bf66b53b304b04cf298f6
otfngo <otfngo@qq.com>
2017-09-20 22:05:23 +0800

    ...

---
+++ requirejs/main.js
@@ -0,0 +1,4 @@
+require(['./a.js'], function (a) {
+    let date = new Date()
+    a.printDate(date)
+})
---
+++ requirejs/util.js
@@ -0,0 +1,12 @@
+define(function () {
+    return {
+        getFormatDate: function (date, type) {
+            if (type === 1) {
+                return '2017-09-20'
+            }
+            if (type === 2) {
+                return '2017年9月20日'
+            }
+        }
+    }
+})
