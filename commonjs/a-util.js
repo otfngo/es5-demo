7a3efed2269d3e497a8b99b97ffe114c6bc5bebf
otfngo <otfngo@163.com>
2018-06-13 07:46:50 +0800

    ...

--- commonjs/a-util.js
+++ commonjs/a-util.js
@@ -1,7 +1,7 @@
 let util = require('./util')
 
 module.exports = {
-    aGetFormatDate: function (date) {
-        return util.getFormatDate(date, 2)
-    }
+  aGetFormatDate: function (date) {
+    return util.getFormatDate(date, 2)
+  }
 }
