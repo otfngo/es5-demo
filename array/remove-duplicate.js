2886f93a1e6360a35522f210211c7b6c409d1ec7
otfngo <otfngo@163.com>
2018-07-01 11:19:47 +0800

    ...

--- array/remove-duplicate.js
+++ array/remove-duplicate.js
@@ -9,16 +9,6 @@ const arr = [1, 22, 22, 333, 333, 333]
 /**
  * 循环
  */
-const forLoop = []
-for (let i = 0, len = arr.length; i < len; i++) {
-  const curr = arr[i]
-  const has = forLoop.includes(curr)
-  if (!has) {
-    forLoop.push(curr)
-  }
-}
-console.log('forLoop', forLoop) // forLoop [1, 22, 333]
-
 const forEachLoop = []
 arr.forEach(item => {
   const has = forEachLoop.includes(item)
@@ -28,6 +18,7 @@ arr.forEach(item => {
 })
 console.log('forEachLoop', forEachLoop) // forEachLoop [1, 22, 333]
 
+
 /**
  * 循环
  * 
@@ -35,26 +26,12 @@ console.log('forEachLoop', forEachLoop) // forEachLoop [1, 22, 333]
  * 循环时，判断当前项与前一项是否相等，相等就说明重复，不相等就添加进新数组
  */
 const sortArr = [...arr].sort()
-
-const optimizedForLoop = []
-let prev = undefined
-for (let i = 0, len = sortArr.length; i < len; i++) {
-  const curr = sortArr[i]
-  const isEqual = Object.is(curr, prev)
-
-  // 对于第0项，若其值恰好为 undefined，则通过比较curr与prev是否相等来判断会有误
-  if (i === 0 || !isEqual) {
-    optimizedForLoop.push(curr)
-    prev = curr
-  }
-}
-console.log('optimizedForLoop', optimizedForLoop) // optimizedForLoop, [1, 22, 333]
-
 const optimizedForEachLoop = []
-prev = undefined
+let prev = undefined
 sortArr.forEach((item, index) => {
   const isEqual = Object.is(item, prev)
 
+  // 对于第0项，若其值恰好为 undefined，则通过比较item与prev是否相等来判断会有误
   if (index === 0 || !isEqual) {
     optimizedForEachLoop.push(item)
     prev = item
@@ -66,4 +43,12 @@ console.log('optimizedForEachLoop', optimizedForEachLoop) // optimizedForEachLoo
  * Set
  */
 const setArr = [...new Set(arr)]
+// const setArr = Array.from(new Set(arr))
 console.log('setArr', setArr) // setArr [1, 22, 333]
+
+/**
+ * Map
+ */
+const seen = new Map()
+const mapArr = arr.filter(item => !seen.has(item) && seen.set(item, 1))
+console.log('mapArr', mapArr) // mapArr, [1, 22, 333]
