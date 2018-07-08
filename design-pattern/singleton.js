23913b77ac76644b5aa2e707af3fa478b242d122
otfngo <otfngo@163.com>
2018-07-08 10:26:43 +0800

    ...

--- design-pattern/singleton.js
+++ design-pattern/singleton.js
@@ -17,3 +17,30 @@ const a = Singleton.getInstance('sven1')
 const b = Singleton.getInstance('sven2')
 
 console.log(a === b)
+
+class CreateDiv {
+  constructor(html) {
+    this.html = html
+    this.init()
+  }
+  init() {
+    let div = document.createElement('div')
+    div.innerHTML = this.html
+    document.body.appendChild(div)
+  }
+}
+
+const ProxySingletonCreateDiv = (function () {
+  let instance
+  return function (html) {
+    if (!instance) {
+      instance = new CreateDiv(html)
+    }
+    return instance
+  }
+})()
+
+const c = new ProxySingletonCreateDiv('sven1')
+const d = new ProxySingletonCreateDiv('sven2')
+
+console.log(c === d)
