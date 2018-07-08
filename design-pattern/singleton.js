b60e828ec1fc1a5fb11e8ea61474ccd6f00dcb9e
otfngo <otfngo@163.com>
2018-07-09 00:26:59 +0800

    ...

--- design-pattern/singleton.js
+++ design-pattern/singleton.js
@@ -44,3 +44,35 @@ const c = new ProxySingletonCreateDiv('sven1')
 const d = new ProxySingletonCreateDiv('sven2')
 
 console.log(c === d)
+
+
+function getSingle(fn) {
+  let result
+  return function() {
+    return result || (result = fn.apply(this, arguments))
+  }
+}
+
+function createLoginLayer() {
+  const div = document.createElement('div')
+  div.innerHTML = '我是登录浮窗'
+  div.style.display = 'none'
+  document.body.appendChild(div)
+  return div
+}
+
+let createSingleLoginLayer = getSingle(createLoginLayer)
+document.getElementById('loginBtn').onclick = () => {
+  const loginLayer = createSingleLoginLayer()
+  loginLayer.style.display = 'block'
+}
+
+let createSingleIframe = getSingle(() => {
+  const iframe = document.createElement('iframe')
+  document.body.appendChild(iframe)
+  return iframe
+})
+document.getElementById('loginBtn').onclick = () => {
+  const loginIframe = createSingleIframe()
+  loginIframe.src = 'http://www.baidu.com'
+}
