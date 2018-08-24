accc3f65b35722d0afdef05e800a9ae1aeb05cd6
otfngo <otfngo@163.com>
2018-08-24 18:52:08 +0800

    ...

--- object/class05.js
+++ object/class05.js
@@ -31,3 +31,36 @@
   const instance2 = new SubType()
   console.log(instance2.colors) // red, blue, green, black
 }
+
+{
+  function SuperType(name) {
+    this.name = name
+    this.colors = ['red', 'blue', 'green']
+  }
+
+  SuperType.prototype.sayName = function() {
+    alert(this.name)
+  }
+
+  function SubType(name, age) {
+    SuperType.call(this, name)
+    this.age = age
+  }
+
+  SubType.prototype = new SuperType()
+
+  SubType.prototype.sayAge = function() {
+    alert(this.age)
+  }
+
+  const instance1 = new SubType('Nicholas', 29)
+  instance1.colors.push('black')
+  console.log(instance1.colors) // red, blue, green, black
+  instance1.sayName() // Nicholas
+  instance1.sayAge() // 29
+
+  const instance2 = new SubType('Greg', 27)
+  console.log(instance2.colors) // red, blue, green
+  instance1.sayName() // Greg
+  instance1.sayAge() // 27
+}
